fe83b1cb2af35b8cc85e6fbd6e7f5404d2670e22
Jared <jared.welch1@gmail.com>
2017-04-20 20:57:27 -0500

    first commit

---
+++ client.py
@@ -0,0 +1,31 @@
+import socket
+import sys
+from _thread import *
+
+host = ''
+port = 5555
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+	s.bind((host, port))
+except socket.error as e:
+	print(str(e))
+
+s.listen(5)
+print('waitin for connection')
+def threaded_client(conn):
+		conn.send(str.encode('Welcome, type your login'))
+
+		while True:
+			data = conn.recv(2048)
+			reply = 'Server output: ' + data.decode('utf-8')
+			if not data:
+					break
+			conn.sendall(str.encode(reply))
+		conn.close()
+
+while True:
+
+	conn, addr = s.accept()
+	print('connected to client')
+	start_new_thread(threaded_client,(conn,))
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Chat Client
+
+This application creates a thread for each new client that connects to the 
+server. Client can use commands createaccount, login, send, and logout. Written
+using python.
