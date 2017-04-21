1ecb0afee4c8e173e310a7113ab2b4270577d041
Jared <jared.welch1@gmail.com>
2017-04-21 09:03:55 -0500

    create username/pass file

---
+++ server.py
@@ -0,0 +1,51 @@
+import socket
+import sys
+from _thread import *
+import json
+
+
+def threaded_client_handler(conn):
+	conn.send(str.encode('Welcome, these commands are available: '))
+
+	while True:
+		data = conn.recv(2048)
+		reply = 'Server output: ' + data.decode('utf-8')
+		if not data:
+				break
+		conn.sendall(str.encode(reply))
+	conn.close()
+
+
+class server(object):
+
+	def __init__(self, host='127.0.0.1', port=5555):
+
+		try:
+			with open('users.json', 'r') as file:
+				users = json.load(file)
+		except:
+			print('error opening user file')
+			exit()
+
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s = self.s
+		try:
+			s.bind((host, port))
+		except socket.error as e:
+			print(str(e))
+
+		s.listen(5)
+		print('waitin for connection')
+
+
+	def begin_server(self):
+		while True:
+
+			conn, addr = self.s.accept()
+			print('connected to client')
+			start_new_thread(threaded_client_handler,(conn,))
+
+
+
+if __name__ == "__main__":
+	server().begin_server()
